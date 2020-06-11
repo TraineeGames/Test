55c0ee454adc75523d5f08bdc77cbcaf283fd9d0
TraineeGames <traineegames@gmail.com>
2020-06-11 13:09:25 +0300

    Upload

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public Vector3 velocity;
+    void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, 0.125f);
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    public Vector3 startPosition, startRotation;
+    public Vector3 firstPosition;
+    public Vector3 lastPosition;
+    public Image powerBar;
+    public float maxPower = 900;
+    private Rigidbody rb;
+    private Vector3 force;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation.eulerAngles;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.AddForce(-force);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            lastPosition = Input.mousePosition;
+            force = lastPosition - firstPosition;
+            force.z = force.y;
+            force.y = 0;
+            
+            var tempMag = force.magnitude;
+            force.Normalize();
+            tempMag = Mathf.Clamp(tempMag, 0, maxPower);
+            force *= tempMag;
+            powerBar.fillAmount = force.magnitude / maxPower;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.zero;
+            transform.position = startPosition;
+            transform.eulerAngles = startRotation;
+        }
+    }
+}
